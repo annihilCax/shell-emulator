4bb33dab4ba6303976726c0d09e695532ad86c8d
coder2 <coder2@email.com>
2024-12-08 20:22:19 +0300

    update README + add parsing of the command

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
+import argparse
 
+def parse_args():
+    parser = argparse.ArgumentParser(description="Shell-эмулятор")
+    parser.add_argument("--username", required=True, help="Имя пользователя")
+    parser.add_argument("--tar", required=True, help="Путь к tar-архиву с виртуальной файловой системой")
+    parser.add_argument("--log", required=True, help="Путь к лог-файлу")
+
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    args = parse_args()
 
