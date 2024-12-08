1d486bfc7caa824e5addcf650b0f3b77e811fc6f
coder2 <coder2@email.com>
2024-12-09 00:59:45 +0300

    add commands

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import tarfile
 import json
 import datetime
 
+
 def parse_args():
     parser = argparse.ArgumentParser(description="Shell-эмулятор")
     parser.add_argument("--username", required=True, help="Имя пользователя")
@@ -11,6 +12,7 @@ def parse_args():
     parser.add_argument("--log", required=True, help="Путь к лог-файлу")
 
     return parser.parse_args()
+
 
 class ShellEmulator:
     def __init__(self, username, tar_path, log_path):
@@ -26,8 +28,44 @@ class ShellEmulator:
         if not os.path.exists(log_path):
             raise FileNotFoundError(f"{log_path}: no such file or directory.")
 
+        #
+        # команды
+        def ls(self):
+            current = self.file_structure
+            for part in self.current_path.strip('/').split('/'):
+                if part:
+                    current = current.get(part, {})
+            print(' '.join(current.keys()))
+
+        def cd(self, path):
+            new_path = self._resolve_path(path)
+            current = self.file_structure
+            for part in new_path.strip('/').split('/'):
+                if part:
+                    current = current.get(part)
+                    if current is None:
+                        print(f"{path}: no such file or directory.")
+                        return
+            self.current_path = new_path
+
+        def mkdir(self, name):
+            current = self.file_structure
+            for part in self.current_path.strip('/').split('/'):
+                if part:
+                    current = current.get(part, {})
+            if name in current:
+                print(f"Cannot create directory '{name}': File exists")
+            else:
+                current[name] = {}
+
+        def history(self):
+            for index, command in enumerate(self.command_history, 1):
+                print(f"{index} {command}")
+
+        def exit(self):
+            print("Exiting...")
+            exit()
 
 
 if __name__ == "__main__":
     args = parse_args()
-
