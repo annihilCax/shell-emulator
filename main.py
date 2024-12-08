545ed907c4bfc070c7f2c50f7960eac10de53ade
coder2 <coder2@email.com>
2024-12-08 22:55:54 +0300

    update README + add initialization of emulator

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 import argparse
+import os
+import tarfile
+import json
+import datetime
 
 def parse_args():
     parser = argparse.ArgumentParser(description="Shell-эмулятор")
@@ -7,6 +11,21 @@ def parse_args():
     parser.add_argument("--log", required=True, help="Путь к лог-файлу")
 
     return parser.parse_args()
+
+class ShellEmulator:
+    def __init__(self, username, tar_path, log_path):
+        self.username = username
+        self.tar_path = tar_path
+        self.log_path = log_path
+        self.current_path = '/'
+        self.command_history = []
+        self.file_structure = {}
+
+        if not os.path.exists(tar_path):
+            raise FileNotFoundError(f"{tar_path}: no such file or directory.")
+        if not os.path.exists(log_path):
+            raise FileNotFoundError(f"{log_path}: no such file or directory.")
+
 
 
 if __name__ == "__main__":
